b04dd8f4f2d41a51a6a6aabb9d2dce81a6467ed0
gnoparus <surapong@gmail.com>
2022-12-22 19:37:25 +0700

    Add random between 0.0-1.0

--- src/114_random-number.py
+++ src/114_random-number.py
@@ -6,6 +6,11 @@ print(random.randrange(1, 10))
 print(random.randrange(1, 10))
 print(random.randrange(1, 10))
 
+# rondom number between 0.0 - 1.0
+print(random.random())
+print(random.random())
+print(random.random())
+
 # random choice
 rps_list = ["rock", "paper", "scissor"]
 rps_choice = random.choice(rps_list)
