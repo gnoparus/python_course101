b5df463dc5a8674ba286c8953da86986ad7aeeb4
gnoparus <surapong@gmail.com>
2022-12-22 18:43:31 +0700

    Add random choice

--- src/114_random-number.py
+++ src/114_random-number.py
@@ -1,4 +1,12 @@
 # random
 import random
 
+# random number between 1 - 9
 print(random.randrange(1, 10))
+print(random.randrange(1, 10))
+print(random.randrange(1, 10))
+
+# random choice
+rps_list = ["rock", "paper", "scissor"]
+rps_choice = random.choice(rps_list)
+print(rps_choice)
