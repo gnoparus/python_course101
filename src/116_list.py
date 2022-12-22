c2697c852312879705722d2ffcecc2300b095b49
gnoparus <surapong@gmail.com>
2022-12-22 20:33:25 +0700

    Add python list

---
+++ src/116_list.py
@@ -0,0 +1,37 @@
+foods = [
+    "Hamburger",
+    "Pizza",
+    "Somtam",
+    "Noodle",
+    "Pad thai",
+    "Steak",
+    "Tom yum",
+    "Tom kha kai",
+]
+
+fruits = [
+    "banana",
+    "apple",
+    "payaya",
+    "durian",
+    "mango",
+    "longan",
+    "lychee",
+    "lemon",
+    "orange",
+    "pineapple",
+]
+
+
+print(foods)
+print(fruits)
+
+print(foods[0])
+print(foods[1])
+print(foods[2])
+
+print(fruits[-1])
+print(fruits[-2])
+print(fruits[-3])
+
+print(foods[0:3])
