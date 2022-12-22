e5bda778a213a93f7d821144404b7da25b9953b6
gnoparus <surapong@gmail.com>
2022-12-22 17:49:26 +0700

    Add number function

---
+++ src/113_numbers.py
@@ -0,0 +1,29 @@
+# int
+print(6 * 3)
+print(type(6))
+
+# float
+print(2.5 * 3)
+print(type(2.5))
+
+# parentheses
+print((0.5 + 9.5) / 3)
+
+# mod
+print(10 % 3)
+print(10 // 3)
+
+
+num = 5.52
+# error only str not float
+# print("num = " + num)
+print(type("hello"))
+print("num = " + str(num))
+print(type(str(num)))
+
+# cast
+print(int(5.1234))
+print(float(5))
+print(str(5.5))
+# print(int("one"))         # error invalid literal
+# print(float("one.five"))         # error invalid literal
