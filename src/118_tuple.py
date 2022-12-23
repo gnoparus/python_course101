e235821bbf32138cc9b0dfea3d41b609f0740d78
gnoparus <surapong@gmail.com>
2022-12-23 11:20:24 +0700

    Add tuple sample

---
+++ src/118_tuple.py
@@ -0,0 +1,29 @@
+iconsiam_coordinate = (13.726294415060304, 100.51036937945553)
+siamparagon_coordinate = (13.746233828725202, 100.53476597754464)
+centralworld_coordinate = (13.745988627621033, 100.53998544365108)
+
+# Print
+print(iconsiam_coordinate)
+print(siamparagon_coordinate)
+print(centralworld_coordinate)
+
+# Open coordinate in Google Maps
+print("Google Maps -------------------------------------------")
+base_url = "https://maps.google.com/?q="
+print(f"IconSiam - {base_url}{iconsiam_coordinate[0]},{iconsiam_coordinate[1]}")
+print(f"SiamParagon - {base_url}{siamparagon_coordinate[0]},{iconsiam_coordinate[1]}")
+print(f"ContralWorld - {base_url}{centralworld_coordinate[0]},{iconsiam_coordinate[1]}")
+
+# List of tuple
+print("List of tuple-------------------------------------------")
+department_stores = [
+    ("IconSiam", (13.726294415060304, 100.51036937945553)),
+    ("SiamParagon", (13.746233828725202, 100.53476597754464)),
+    ("CentralWorld", (13.745988627621033, 100.53998544365108)),
+]
+
+print(department_stores)
+print(department_stores[0])
+print(department_stores[0][0])
+print(department_stores[0][1])
+print(department_stores[0][1][0])
