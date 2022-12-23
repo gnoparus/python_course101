13f3b3f418326c828d05c2f44582e42a9997eeb5
gnoparus <surapong@gmail.com>
2022-12-23 11:24:26 +0700

    Add print type of tuple

--- src/118_tuple.py
+++ src/118_tuple.py
@@ -3,13 +3,14 @@ siamparagon_coordinate = (13.746233828725202, 100.53476597754464)
 centralworld_coordinate = (13.745988627621033, 100.53998544365108)
 
 # Print
+print(type(iconsiam_coordinate))
 print(iconsiam_coordinate)
 print(siamparagon_coordinate)
 print(centralworld_coordinate)
 
 # Open coordinate in Google Maps
 print("Google Maps -------------------------------------------")
-base_url = "https://maps.google.com/?q="
+base_url = "https://www.google.com/maps/search/?api=1&query="
 print(f"IconSiam - {base_url}{iconsiam_coordinate[0]},{iconsiam_coordinate[1]}")
 print(f"SiamParagon - {base_url}{siamparagon_coordinate[0]},{iconsiam_coordinate[1]}")
 print(f"ContralWorld - {base_url}{centralworld_coordinate[0]},{iconsiam_coordinate[1]}")
