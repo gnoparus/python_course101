2d7bcd98588611bfd1f28971d9a81051655c75a4
gnoparus <surapong@gmail.com>
2022-12-25 20:15:15 +0700

    Add comparison

--- src/125_comparison.py
+++ src/125_comparison.py
@@ -1,7 +1,7 @@
 def max(a, b, c):
-    if a >= b and b >= c:
+    if a >= b and a >= c:
         return a
-    elif b >= a and a >= c:
+    elif b >= a and b >= c:
         return b
     elif c >= a and c >= b:
         return c
@@ -10,3 +10,4 @@ def max(a, b, c):
 print(max(4, 5, 6))
 print(max(-5, 5, -10))
 print(max(0.5, 15, 33))
+print(max(55, 33, 44))
