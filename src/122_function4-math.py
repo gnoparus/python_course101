b95ff20bf675a44ca385e98e00fb7e4617ff0ef4
gnoparus <surapong@gmail.com>
2022-12-25 16:30:22 +0700

    Add sample math functions

---
+++ src/122_function4-math.py
@@ -0,0 +1,42 @@
+import math
+import statistics
+
+print(math.pow(2, 10))
+print(math.sqrt(81))
+
+print(math.pi)
+print(math.sin(math.pi / 4))
+print(math.cos(0))
+
+print(statistics.mean([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+print(statistics.median([3, 4, 5, 6, 99]))
+
+
+# The math module has a set of methods and constants.
+
+# Method	Description
+# math.exp()	Returns E raised to the power of x
+# math.log()	Returns the natural logarithm of a number, or the logarithm of number to base
+
+# math.sqrt()	Returns the square root of a number
+# math.pow()	Returns the value of x to the power of y
+
+# math.sin()	Returns the sine of a number
+# math.cos()	Returns the cosine of a number
+# math.tan()	Returns the tangent of a number
+
+# math.gcd()	Returns the greatest common divisor of two integers
+# math.lcm() Return the least common multiple of the specified integer arguments.
+
+# Constant	Description
+# math.e	Returns Euler's number (2.7182...)
+# math.inf	Returns a floating-point positive infinity
+# math.nan	Returns a floating-point NaN (Not a Number) value
+# math.pi	Returns PI (3.1415...)
+
+
+# Method	Description
+# statistics.mean()	Calculates the mean (average) of the given data
+# statistics.median()	Calculates the median (middle value) of the given data
+# statistics.mode()	Calculates the mode (central tendency) of the given numeric or nominal data
+# statistics.stdev()	Calculates the standard deviation from a sample of data
