94f4b62aa7d8ee0b18cacdc52a791a0142993dc1
gnoparus <surapong@gmail.com>
2022-12-25 17:02:59 +0700

    Add custom function call custom function

---
+++ src/123_function5-return.py
@@ -0,0 +1,36 @@
+import math
+
+
+def area_square(length):
+    area = length * length
+    return area
+    print("Returning Area!")
+
+
+def area_circle(radius):
+    area = math.pi * radius**2
+    return area
+
+
+def volume_pringles(radius, height):
+    area = area_circle(radius)
+    volume = area * height
+    return volume
+
+
+length1 = 5
+area1 = area_square(length1)
+print(f"area1 = {area1}")
+
+length2 = 7
+area2 = area_square(length=length2)
+print(f"area2 = {area2}")
+
+radius3 = 6
+area3 = area_circle(radius3)
+print(f"area3 = {area3:.2f} cm²")
+
+radius4 = 6
+height4 = 25
+volume4 = volume_pringles(radius4, height4)
+print(f"volume4 = {volume4:10.2f} cc.")
