de0edacb57bd166de5a30d755bb35b3fe8c47423
gnoparus <surapong@gmail.com>
2022-12-26 15:07:59 +0700

    Add for loop

---
+++ src/129_loop-for.py
@@ -0,0 +1,21 @@
+text1 = "Last Christmas I gave you my heart"
+
+for char in text1:
+    print(char)
+
+for word in text1.split(" "):
+    print(word)
+
+
+# list2 = "But the very next day you gave it away".split(" ")
+list2 = ["But", "the", "very", "next", "day", "you", "gave", "it", "away"]
+
+for word in list2:
+    print(word)
+
+# list2_length = len(list2)
+list2_length = 9
+print(f"list2_length = {list2_length}")
+
+for i in range(list2_length):
+    print(f"i = {i}, list2[i] = {list2[i]}")
