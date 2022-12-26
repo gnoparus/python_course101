3bad16814ec7a03f8c6ffd910df704961664c01b
gnoparus <surapong@gmail.com>
2022-12-26 13:58:40 +0700

    Refactor to input in loop

--- src/127_loop-while.py
+++ src/127_loop-while.py
@@ -6,14 +6,13 @@ import random
 #     print("infinite loop")
 
 secret = random.randrange(1, 100)
-guess = int(input("Please guess number 1-100: "))
+guess = -1
 
 while guess != secret:
+    guess = int(input("Please guess number 1-100: "))
     if guess > secret:
         print("Too high")
     elif guess < secret:
         print("Too low")
-
-    guess = int(input("Please guess number again: "))
 
 print("You win")
