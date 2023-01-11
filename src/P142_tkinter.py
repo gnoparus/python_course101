651995b334e8d479e577575d8963bb214d6d08e7
gnoparus <surapong@gmail.com>
2023-01-11 12:17:44 +0700

    Add binded event

--- src/P142_tkinter.py
+++ src/P142_tkinter.py
@@ -1,9 +1,15 @@
 import tkinter as tk
 
+
+def button_clicked():
+    new_label = tk.Label(window, text="Hello " + entry.get())
+    new_label.pack()
+
+
 window = tk.Tk()
 label = tk.Label(window, text="Hello Tkinter")
 entry = tk.Entry(window)
-button = tk.Button(window, text="OK")
+button = tk.Button(window, text="OK", command=button_clicked)
 
 label.pack()
 entry.pack()
